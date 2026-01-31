f64d9b2d64cc217d33734ca045c4accf729cb8a3
Dhairya-Shah-1 <dhairyashah1710@gmail.com>
2026-02-01 02:27:31 +0530

    Delete Project v5 directory

--- Project v5/src/components/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://sbbfbzytpshfnlhymkss.supabase.co';
-const supabaseKey = 'sb_publishable_US_0_m1eMLhIjq2DjGDpdA_z98L6FDQ';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
